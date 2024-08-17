50b234cf0ebf08fa0cec67ef041a6619d35a52ae
Mitesh <dev.miteshgodne@gmail.com>
2024-08-17 16:06:12 +0530

    Variables

---
+++ 1_Introduction/5_Variables.py
@@ -0,0 +1,32 @@
+''' Variables -> Container that stores some data.'''
+# _variable_name = value
+# $ cannot be used to start variable name
+
+# Multiple Assignment
+a,b,c = 10,20,30
+print(a,b,c)
+
+# One value to Multiple Variables
+a = b = c = 20
+print(a,b,c)
+
+# Unpack a Collection
+list1 = ["hello", "world"]
+x,y = list1
+print(x,y)
+
+# Type Error - Not Allowed to add incompatible object using +
+# cantRun = "Abeer" + 5  # Type Error
+
+# global keyword is used to access global variable inside a function with local variable of same name
+
+'''Reference of another object'''
+x = 10
+y = x
+y = 30
+print(x)
+print(y)
+
+'''Constants are not applicable'''
+# It is a convention to denote constants in CAPITALS
+PI = 3.14
