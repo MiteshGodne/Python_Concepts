b9796a0619349e3d8d3c517268b6837509db91f1
Mitesh <dev.miteshgodne@gmail.com>
2024-08-17 16:03:44 +0530

    Introduction to Python

---
+++ 1_Introduction/1_Intro.py
@@ -0,0 +1,77 @@
+print("Hello Python")
+
+# History of Python => 
+# 1. Python is a dynamically typed general purpose object oriented programming language made by Dutch Guido van Rossum in 1989 at NRI Netherlands, but was made available to public in Feb 20th 1991.
+# 2. Named after monty python circus TV show.
+# 3. Python repl = Read Evaluate Print Loop
+
+
+# Major Versions Of Python =>
+# 1. Python 0.9.0 - (Feb 1991) included exception handling, functions, data types, etc.
+# 2. Python 1.0 - (Jan 1994) included functional programming, lambda, map, filter and reduce.
+# 3. Python 2.0 - (Oct 2000) included list comprehensions, garbage collector and unicode support.
+# 4. Python 3.0 / Py3k / 3000 - (Dec 2008) not backward compatible with 2.x
+# 5. Python 3.12.1 - current version
+
+
+# Features of Python =>
+# 1. Simple and easy to learn.
+# 2. Platform Independent due to PVM.
+# 3. Freeware & Open Source.
+# 4. Big Library Support.
+# 5. Integration with other programming language is possible.
+# 6. Case Sensitive.
+# 7. Both Procedure and Object Oriented.
+# 8. Dynamically Typed.
+# 9. Interpreted Language & Compilation done by Python Interpreter.
+
+
+# Applications of Python => 
+# 1. Data Visualization - to create plots and graphical representation.
+# 2. Data Analytics - to analyse data.
+# 3. Ai and ML Development.     
+# 4. Development of Web Applications.
+# 5. Handle Databases.
+# 6. Desktop, Games and Database Applications.
+# 7. Network Programming & IOT.
+
+
+# Limitations of Python =>
+# 1. Not up to the mark because Interpreted language.
+# 2. Not suitable for Mobile App Development.
+
+
+# Flavours of Python =>
+# 1. CPython - works with C
+# 2. JPython - works with Java
+# 3. IronPython - works with C# and DotNet
+# 4. PyPy - improved performance due to JIT compiler inside PVM.
+# 5. RubyPython - works with Ruby platform.
+# 6. AnacondaPython - specially designed to handle large volume of data processing
+
+
+# Python 2                         ||          Python 3
+# print is a statement             ||  # print is a function
+# strings are stored as ASCII      ||  # strings are stored as UNICODE
+# int division gives int result    ||  # int division can give float result  
+# exceptions enclosed in notations ||  # exceptions are enclosed in ()
+# global var changes in for loop   ||  # global var value never changes
+# xrange() is used                 ||  # range() is used
+# raw_input() is [in str only]     ||  # input() is used only
+# long data type was present       ||  # long is not present
+
+
+
+# Pylance is a python language server.
+
+
+# Modules in Python => Code Blocks already written by others.
+# 1. External - Needs to be imported explicitly from 3rd party using "import" keyword.
+# 2. Built In - Already associated with python interpreter.
+
+
+# PIP => Performance improvement plan is a package management system used to install & manage software packages.
+# 1. Commands =>
+# >>> python : to check if python is installed and opens a REPL (Read Evaluate Print Loop) which executes python line by line  
+# >>> python --version : to check python version 
+# >>> pip install package_name : to install software modules/package
