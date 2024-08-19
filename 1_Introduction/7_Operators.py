25270d4c93b15ceaeb19e3139f3465df1cd308c0
Mitesh <dev.miteshgodne@gmail.com>
2024-08-19 16:06:57 +0530

    Operators

---
+++ 1_Introduction/7_Operators.py
@@ -0,0 +1,110 @@
+# Operators = Operators are used to perform operations on operands
+
+
+''' Arithmetic Operators 
+# +, -, *, /, %, 
+# Integer & Floor Division (//)
+# Exponential (**)
+'''
+
+
+
+'''Bitwise Operators
+# { &  |  ^  ~ }
+# { <<  >> }
+'''
+
+
+''' Assignment Operators   
+# Arithmetic {  =  -=   +=  /=  *=  %=  //=  **= }
+# Bitwise {  &=  |=  ^=  }
+# Shift { >>=  <<= }
+# Set Equal to { print(x : = 3) --> x = 3; print(x) }  
+'''
+
+
+'''Repetition Operator - When * is used between one string and one integer value n, it acts as repetition operator and the string is repeated n times'''
+print("Hello "*3)
+
+
+'''Identity Operator - It is used to check if the objects are same with same reference and not if they are equal. 
+'''
+name = "Abeer"
+myName = "Abeer"
+
+# a]  is -> Same Object  
+print(name is myName)    # True
+
+# b]  is not  
+print(name is not myName)   # False
+
+# Note - Value same but Objects are different
+list1 = [1,2,3]
+list2 = [1,2,3]
+print(list1 is list2)   # False
+
+# Reference value of an object
+print(id(list1))
+
+
+'''Membership Operator - It is used to check if the sequence with the specified value is present in the object or not. 
+'''
+data = ["Abeer", "Sharma", 21]
+
+# a]  in -> Same Object 
+print("Abeer" in data)     # True
+
+# b]  not in  
+print("Abeer" not in myName)    # False
+
+
+
+'''Comparison Or Relational Operators 
+{ == >=  <=  <  >  != }
+'''
+# Strings can also be compared on Unicode basis. 
+# Chaining of relational operators is possible.
+print(10<20<30<40)    # returns True
+print(10>True)  # True
+print((10<20<30)==True)
+print(("") != False)
+a = 2
+b = 3
+print(a or b)
+
+
+'''Ternary Operator'''
+x = 30 if a<b else 40
+print(x)
+# Nested Ternary Operator
+min = a if a<b and a<x else b if b<x else x
+
+
+
+'''Conditional Operator 
+{ and  or  not }
+'''
+
+
+''' Slicing Operator [] => string[ startIdx : endIdx ] '''
+str = "Hello"
+print(str[0:40])  # no error
+print(str[40])  # error
+
+
+
+'''Operator Precedence'''
+# () 
+# **
+# unary , ~ 
+# *, /, //, %
+# +, -
+# shift
+# relational
+# &
+# ^
+# |
+# Comparison, Identity, Membership 
+# not
+# and
+# or
