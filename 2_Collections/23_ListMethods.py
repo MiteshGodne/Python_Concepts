a1c44ad6e5a67201a052d35c74de8d2e0ad2d6ac
Mitesh <dev.miteshgodne@gmail.com>
2024-08-26 20:18:59 +0530

    Tuple Methods and Operations

--- 2_Collections/23_ListMethods.py
+++ 2_Collections/23_ListMethods.py
@@ -8,7 +8,7 @@ max(l)
 # Accessing list
 l.count(10)
 l.index(30)    # returns index of element else Value Error
-
+l.index(30,2,4) # (ele, start, stop) for searching in a sub part
 
 # Add elements
 l.append(50)   # add element at last
@@ -22,20 +22,23 @@ l.remove(30)    # first occurrence  else  Value Error
 l.pop()   # returns last element  else Index Error
 l.pop(2)   # removes index 2 element
 l.clear()   # removes all the elements and makes it an empty list
-del l2[2]  # deletes the list / index element else Index Error
+# del l2[2]  # deletes the list / index element else Index Error
 
 
 
-# Alphanumeric Sorting using in-built sort() function
+# Alphanumeric Sorting using in-built sort() function which performs inplace sorting
 l.reverse()
 l.sort()   # Ascending Sort else Type Error in Heterogeneous List
+
+# Sorting is case sensitive (capitals then smalls), to make it in-sensitive we can use custom sorting key keyword
+l.sort(key = str.lower)
+
 l.sort(reverse = True)  # Descending Sort
+
 # Custom Sorting using a function
 def myFunc(n):
     return n-50
 l.sort(key = myFunc) 
-# Sorting is case sensitive (capitals then smalls), to make it in-sensitive we can use custom sorting key keyword
-l.sort(key = str.lower())
 
 
 # List __add__ function : returns the combined lists 
