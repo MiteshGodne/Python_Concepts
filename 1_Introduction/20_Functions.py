3358677016465bc20fc5535dd82054ac498d416e
Mitesh <dev.miteshgodne@gmail.com>
2024-08-29 16:33:51 +0530

    Functions in Python

--- 1_Introduction/20_Functions.py
+++ 1_Introduction/20_Functions.py
@@ -3,13 +3,14 @@
 # A] Built in Functions ->
 ''' print(),id(),input(), range(), min(), max(), len()'''
 
-# B] User Defined Functions -> 
+# B] User Defined Functions -> using def keyword 
 ''' functions that user defines and calls'''
 num1 = 9
 num2 = 8
 def geo_mean():
     return (num1*num2)/(num1+num2)
 print(f"Harmonic mean is : {geo_mean()}")
+
 
 '''Declaration of Function'''
 # pass keyword -> 
@@ -28,73 +29,7 @@ later()
  => function returns None if return is not used'''
 
 
-
-# Parameters can be ->
-# 1] Actual Parameters - Defined and used within the main program.
-# 2] Formal Parameters - Defined and used within the function.
-
- 
-# Arguments / Actual Parameters can be ->
-
-# 1] Default Arguments - default values
-'''
- => default values which are used if function is called without arguments.'''
-def defaultArgs(a = 0, b = 0):
-    print(a+b)
-defaultArgs()    # both default values are used
-defaultArgs(5)   # default value of b is used
-defaultArgs(b=9) # default value of a is used 
-
-# In python positional parameters 
-
-# 2] Keyword Arguments - Position/order not required
-'''
- => Arguments can be passed in key=value form so that interpreter can identify it by parameter name & hence order is not necessary. 
- => this way we can change the order of arguments passed'''
-def keywordArgs(name, age):
-    print(name,"is",age,"years old")
-keywordArgs(age=21,name="Abeer")
-
-
-# 3] Positional arguments == Required Arguments in order -
-'''
- => These are parameters which are passed in correct positional order.
- => when default values are not defined in the parameters than arguments becomes required arguments and interpreter will throw error if args not given.'''
-def hey(name, age):
-    print(name,age)
-hey("Abeer", 21)
-
-
-# 4] Variable Length Arguments - 
-'''
- => In python we can pass n number of parameters 
- => used when we need to pass more arguments than those defined in the function, 2 ways are there to do so :-'''
-
-# 4.a] Arbitrary Arguments - args as tuple
-'''
- => pass * before the parameters in the function while defining. 
- => In this way the function accesses the arguments in the form of a "tuple".'''
-def arbitraryFunc(* num):
-    print(num[0], num[1])
-arbitraryFunc(10,20)
-
-#4.b] Keyboard Arbitrary Arguments - args as dict
-'''
- => pass ** before the parameters in the function while defining.  
- => In this way the function accesses the arguments in the form of a "dictionary".'''
-
-def keyboardArbFunc(** items):
-    print(items['fname'],items['lname'], "is a good guy")
-keyboardArbFunc(fname="Abeer", lname = "Sharma")
-
-#OR
-
-def function(** dicts):
-    print(dicts)
-function(name="Abeer",age=21,sex="M")
-
-
-# Anonymous Function / Lambda Function : Sometimes we can define the function without name, such functions are called lambda function / anonymous functions / nameless function / unnamed functions.Defined using lambda keyword. It cannot be of multiple lines.
+# Anonymous Function / Lambda Function : Sometimes we can define the function without name, such functions are called lambda function / anonymous functions / nameless function / unnamed functions. Defined using lambda keyword. It cannot be of multiple lines.
 
 # Syntax ->> lambda argument_list : expression
 sum = lambda a,b : a+b
