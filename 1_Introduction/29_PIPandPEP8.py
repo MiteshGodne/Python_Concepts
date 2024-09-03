bf9cecfca4647bb03ab4fdb70d9435694a883565
Mitesh <dev.miteshgodne@gmail.com>
2024-09-03 14:59:06 +0530

    PIP vs PEP8

---
+++ 1_Introduction/29_PIPandPEP8.py
@@ -0,0 +1,24 @@
+# print("PEP 8")
+'''
+>>> Python Enhancement Proposal 8
+>>> PEP 8 is a document that provides guidelines on how to write python code.
+>>> It was written in 2001 by Guido Van Rossum, Barry Warsaw and Nick Coghlan.
+'''
+
+# The Zen of Python by Tim Peters->
+'''
+>>> Tim Peters wrote a 20 aphorisms or lines that guides on how to write python code efficiently. 
+>>> This can be printed in Python REPL by writing - 
+>>> import this 
+>>> print(this)
+'''
+
+
+# PIP => Performance improvement plan is a package management system used to install & manage software packages.
+# 1. Commands =>
+'''>>> python : to check if python is installed and opens a REPL (Read Evaluate Print Loop) which executes python line by line  
+>>> python --version : to check python version 
+>>> pip --version : to check pip version 
+>>> pip install package_name : to install software modules/package
+>>> pip uninstall package_name : to uninstall software modules/package
+>>> pip list: to list all software modules/package installed on system'''
