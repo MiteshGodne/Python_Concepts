9b0ceb1da3e9c4985142d71cbc1f710d43eb5e45
Mitesh <dev.miteshgodne@gmail.com>
2024-10-06 00:34:58 +0530

    Commands for Virtual environment

---
+++ 3_Concepts/43_VirtualEnvironment.py
@@ -0,0 +1,33 @@
+# Virtual Environment - 
+'''
+>>> It is a tool used to isolate specific Python Environment on a single machine, allowing us to work on multiple projects with different dependencies and packages and their versions to prevent conflicts between packages that does not support each other.
+'''
+
+# Commands -
+''' 
+-> venv module is used to create virtual env. & a folder is created with the specified name.
+>>> python3 -m venv myEvtName 
+
+-> activate virtual environment
+>>> myEvtName\Scripts\activate.bat  
+>>> myEvtName\Scripts\activate.ps1
+
+-> now we can install different packages with different versions
+>>> pip install pandas==1.4.4
+
+-> to deactivate activated virtual environment
+>>> deactivate 
+''' 
+
+
+# requirement.txt - file that contains packages and their versions required in the virtual environment
+'''
+1) displays all packages & versions
+>>> pip freeze 
+
+2) creates the requirements.txt file and adds all the packages and versions installed
+>>> pip freeze > requirements.txt 
+
+3) installs all the packages from requirements.txt file
+>>> pip install -r requirements.txt 
+'''
