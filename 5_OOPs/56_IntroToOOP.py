e85bc907a7aec745bdacac127a3e355785b15390
Mitesh <dev.miteshgodne@gmail.com>
2024-10-09 19:05:33 +0530

    Inheritance

--- 5_OOPs/56_IntroToOOP.py
+++ 5_OOPs/56_IntroToOOP.py
@@ -22,8 +22,12 @@ a = Person()
 # Accessing variables using object.var
 print(a.name, a.age, a.occupation)
 
+# Calling object using class name and passing object as argument
+Person.info(a)   # if self is not passed in class methods then this object passed as argument will throw TypeError.
+
 # Calling methods using object.method() 
-a.info()
+a.info()   # converted to >>>Person.info(a)
+
 
 # Modifying object variables
 a.name = "Lokesh"
@@ -39,4 +43,20 @@ del a
 ''' self parameter ->
 >>> self parameter is a reference to the current instance of the class, and it is used to access the variables that belongs to the class.
 >>> It does not have to be named self, you can call it whatever you like, but it has to be the first parameter of any function in the class.
+>>> variables created using constructor are only available to the instance on which it is called.
 '''
+
+# Creating list of objects - 
+class Employee:
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
+    def showDetails(self):
+        print(f"Name : {self.name},  ID : {self.id}")
+
+li = list()
+for i in range(5):
+    li.append(Employee(input("Enter Name and ID : "),101+i ))
+    
+for i in li:
+    i.showDetails()
