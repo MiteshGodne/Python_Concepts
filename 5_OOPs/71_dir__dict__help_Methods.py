5d160abb680bacc347046d0c69eef7da90e9f380
Mitesh <dev.miteshgodne@gmail.com>
2024-10-12 19:00:04 +0530

    dir(), __dict__() and help() method

---
+++ 5_OOPs/71_dir__dict__help_Methods.py
@@ -0,0 +1,20 @@
+# dir() - This method is used for object introspection. It returns the " list of methods, dunder methods and attributes " of an object.
+z = [1,2,3]
+print(dir(z)) 
+t = (2,3,5)
+print(dir(t))
+
+# __dict__ - This attribute returns a dictionary representation of an object's attribute. It is used for introspection where all items assigned using self is shown. Dictionary for instance variables.
+class Person:
+    def __init__(self, age):
+        self.age = age
+    def greet():
+        print("Hello")    
+    
+p = Person(22)
+print(p.__dict__)       # {'age' : 22}
+
+
+# help() - This method is used to print the help documentation for an object, including description of its attributes and methods. It returns None.
+help(p)
+   
