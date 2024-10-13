2dc05feb8040af3fd0e64990d21b8e328c63761e
Mitesh <dev.miteshgodne@gmail.com>
2024-10-13 23:24:16 +0530

    Method Overriding

---
+++ 5_OOPs/74_MethodOverriding.py
@@ -0,0 +1,27 @@
+# Method Overriding - Redefine parent class method in child class
+
+class Shape:
+    def __init__(self, x , y=0):
+        self.x = x
+        self.y = y
+    def area(self):
+        return self.x * self.y
+
+# using parents method
+class Rect(Shape):
+    pass
+
+rect = Rect(3,5)
+print(rect.area())
+
+
+# Overriding area() of parent in class
+class Circle(Shape):
+    def __init__(self, r):
+        return super().__init__(r,r)
+    def area(self):
+        return 3.14 * super().area()
+
+circ = Circle(10)
+print(circ.area())
+
