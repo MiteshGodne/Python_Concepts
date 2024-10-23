6af7d1803824fe3891bb58635e17d32fb3c052d2
Mitesh <dev.miteshgodne@gmail.com>
2024-10-23 17:06:48 +0530

    Practice

---
+++ 6_Practise/MaxOf3.py
@@ -0,0 +1,8 @@
+def greater(num1,num2,num3):
+    if(num1>num2 and num1>num3):
+        print(num1, "is greater")
+    elif(num2>num3):
+        print(num2, "is greater")
+    else:
+        print(num3, " is greater")
+greater(10,20,30)
