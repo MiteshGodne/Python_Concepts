0d1abacbc2c90cc7460745cf1c3a0da81e504f9b
Mitesh <dev.miteshgodne@gmail.com>
2026-01-06 12:11:37 +0530

    Typo fix

--- 5_OOPs/57_Generators.py
+++ 5_OOPs/57_Generators.py
@@ -1,6 +1,6 @@
 # Generator is a function which is used to generate the sequence of the numbers. It pause the execution of the function return the value to the caller and again starts the execution of the function. It uses yield statement instead of return function.
-# return statement executes one time but yield function executes multiple times.
 
+# Normal function with return statement : return statement executes one time
 def myFunc(n):
     for i in range(n):
     #    print(i)
@@ -8,10 +8,11 @@ def myFunc(n):
 res = myFunc(5)
 print(res)
 
+
+# Generator function with yield statement : yield function executes multiple times.
 def myFunc(n):
     for i in range(n):
        yield i
-
-# res stores multiple values so either convert it into list of use for loop to print all the values
+# res stores multiple values so either convert it into list or use for loop to print all the values
 res = list(myFunc(5))
 print(res)
